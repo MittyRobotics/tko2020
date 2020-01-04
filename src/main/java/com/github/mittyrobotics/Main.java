f7c172235e13bcf856a8c918b09d9816e5a566e2
Ashvin Iyer <ashviniyer21@mittymonarch.com>
2020-01-04 15:48:47 -0800

    Fixing Package Names

---
+++ src/main/java/com/github/mittyrobotics/Main.java
@@ -0,0 +1,13 @@
+package com.github.mittyrobotics;
+
+import edu.wpi.first.wpilibj.RobotBase;
+//DO NOT TOUCH
+public final class Main {
+  private Main() {
+
+  }
+
+  public static void main(String... args) {
+    RobotBase.startRobot(Robot::new);
+  }
+}
