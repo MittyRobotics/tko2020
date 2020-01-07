77ebf5383e8631047c8a153e5c13f0b915c0a999
Owen Leather <43189206+OLeather@users.noreply.github.com>
2020-01-07 13:28:58 -0800

    update copyrights

--- src/main/java/com/github/mittyrobotics/autonomous/commands/Translate2DTrajectory.java
+++ src/main/java/com/github/mittyrobotics/autonomous/commands/Translate2DTrajectory.java
@@ -1,7 +1,30 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Mitty Robotics (Team 1351)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.github.mittyrobotics.autonomous.commands;
 
 import com.github.mittyrobotics.datatypes.motion.DrivetrainVelocities;
-import com.github.mittyrobotics.datatypes.motion.DrivetrainWheelVelocities;
 import com.github.mittyrobotics.datatypes.positioning.Transform;
 import com.github.mittyrobotics.path.following.PathFollower;
 import com.github.mittyrobotics.path.following.util.Odometry;
@@ -9,21 +32,21 @@ import com.github.mittyrobotics.path.following.util.PathFollowerProperties;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-public class Translate2DTrajectory extends CommandBase {
+public class Translate2dTrajectory extends CommandBase {
     private PathFollower pathFollower;
     private double previousTime;
 
-    public Translate2DTrajectory(PathFollower pathFollower){
+    public Translate2dTrajectory(PathFollower pathFollower){
         this.pathFollower = pathFollower;
     }
 
-    public Translate2DTrajectory(Transform goal, PathFollowerProperties properties,
+    public Translate2dTrajectory(Transform goal, PathFollowerProperties properties,
                                  PathFollowerProperties.PurePursuitProperties purePursuitProperties){
         PathFollower pathFollower = new PathFollower(properties,purePursuitProperties);
         pathFollower.setDrivingGoal(goal);
     }
 
-    public Translate2DTrajectory(Transform goal, PathFollowerProperties properties,
+    public Translate2dTrajectory(Transform goal, PathFollowerProperties properties,
                                  PathFollowerProperties.RamseteProperties ramseteProperties){
         PathFollower pathFollower = new PathFollower(properties,ramseteProperties);
         pathFollower.setDrivingGoal(goal);
