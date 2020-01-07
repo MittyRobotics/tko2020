98239e75f2aa3b666c427bb38774b4b8a04ef4a5
oliverchen23 <54689665+oliverchen23@users.noreply.github.com>
2020-01-07 15:29:56 -0800

    Update Robot.java

--- src/main/java/com/github/mittyrobotics/Robot.java
+++ src/main/java/com/github/mittyrobotics/Robot.java
@@ -79,8 +79,10 @@ import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.github.mittyrobotics.motionprofile.TrapezoidalMotionProfile;
 import edu.wpi.first.wpilibj.*;
-import edu.wpi.first.wpilibj2.command.PIDSubsystem;
 
+import edu.wpi.first.wpilibj2.command.PIDSubsystem;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 public class Robot extends TimedRobot {
 
@@ -90,6 +92,7 @@ public class Robot extends TimedRobot {
 
   @Override
   public void robotInit() {
+
 
 
 
