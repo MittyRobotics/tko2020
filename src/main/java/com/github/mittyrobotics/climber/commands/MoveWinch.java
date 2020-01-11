c5469aeed94a1462baf5bab448bc1a90a8a22bd0
SameerRao <sameerrao22@gmail.com>
2020-01-11 14:58:11 -0800

    .

--- src/main/java/com/github/mittyrobotics/climber/commands/MoveWinch.java
+++ src/main/java/com/github/mittyrobotics/climber/commands/MoveWinch.java
@@ -37,16 +37,31 @@ public class MoveWinch extends CommandBase {
 
     @Override
     public void execute(){
+//        double tempPos;
+//        final double RAMP_RATE = 10;
+//        if (side == RobotSide.LEFT) {
+//            tempPos = Math.min(pos - Winch.getInstance().getLeftEncoder().getPosition(),
+//                    Winch.getInstance().getLeftEncoder().getPosition() + RAMP_RATE);
+//        } else {
+//            tempPos = Math.min(pos - Winch.getInstance().getRightEncoder().getPosition(),
+//                    Winch.getInstance().getRightEncoder().getPosition() + RAMP_RATE);
+//        }
+//            controller.setReference(tempPos, ControlType.kPosition);
         double tempPos;
         final double RAMP_RATE = 10;
         if (side == RobotSide.LEFT) {
-            tempPos = Math.min(pos - Winch.getInstance().getLeftEncoder().getPosition(),
-                    Winch.getInstance().getLeftEncoder().getPosition() + RAMP_RATE);
-        } else {
-            tempPos = Math.min(pos - Winch.getInstance().getRightEncoder().getPosition(),
-                    Winch.getInstance().getRightEncoder().getPosition() + RAMP_RATE);
-        }
+            tempPos = Winch.getInstance().getLeftEncoder().getPosition() + RAMP_RATE;
+            if (RAMP_RATE > (pos - Winch.getInstance().getLeftEncoder().getPosition())) {
+                tempPos = pos;
+            }
             controller.setReference(tempPos, ControlType.kPosition);
+        } else {
+            tempPos = Winch.getInstance().getRightEncoder().getPosition() + RAMP_RATE;
+            if (RAMP_RATE > (pos - Winch.getInstance().getRightEncoder().getPosition())) {
+                tempPos = pos;
+            }
+            controller.setReference(tempPos, ControlType.kPosition);
+        }
     }
 
     @Override
