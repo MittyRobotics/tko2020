a6deec44e2b54573d6ad546671c045ec09480393
SameerRao <sameerrao22@gmail.com>
2020-01-10 17:50:48 -0800

    wrote ramping functions and initialized hardware for winch

--- src/main/java/com/github/mittyrobotics/Subsystems/Winch.java
+++ src/main/java/com/github/mittyrobotics/Subsystems/Winch.java
@@ -3,7 +3,9 @@ package com.github.mittyrobotics.Subsystems;
 import com.github.mittyrobotics.Constants;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
+import com.revrobotics.ControlType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.CANPIDController;
 
 public class Winch extends SubsystemBase {
 
@@ -11,6 +13,7 @@ public class Winch extends SubsystemBase {
 
     private CANSparkMax leftSpark;
     private CANSparkMax rightSpark;
+    private CANPIDController leftController, rightController;
 
     public Winch() {
         super();
@@ -27,8 +30,33 @@ public class Winch extends SubsystemBase {
         leftSpark.restoreFactoryDefaults();
         rightSpark.restoreFactoryDefaults();
 
+        leftController = new CANPIDController(leftSpark);
+        leftController.setP(0.5);
+        leftController.setI(0);
+        leftController.setD(0);
+        leftController.setOutputRange(-.5, .5);
+
+        rightController = new CANPIDController(leftSpark);
+        rightController.setP(0.5);
+        rightController.setI(0);
+        rightController.setD(0);
+        rightController.setOutputRange(-.5, .5);
     }
 
-    
+    public void moveLeftWinch(double position) {
+        while (Math.abs(position - leftSpark.getAlternateEncoder().getPosition()) < 1) {
+            double tempPos = Math.min(position - leftSpark.getAlternateEncoder().getPosition(),
+                    leftSpark.getAlternateEncoder().getPosition() + 10);
+            leftController.setReference(tempPos, ControlType.kPosition);
+        }
+    }
+
+    public void moveRightWinch(double position) {
+        while (Math.abs(position - rightSpark.getAlternateEncoder().getPosition()) < 1) {
+            double tempPos = Math.min(position - rightSpark.getAlternateEncoder().getPosition(),
+                    rightSpark.getAlternateEncoder().getPosition() + 10);
+            rightController.setReference(tempPos, ControlType.kPosition);
+        }
+    }
 
 }
