6a45e2cfe97f1f89535645aa521ed2dba1e447d3
SameerRao <sameerrao22@gmail.com>
2020-01-10 18:59:08 -0800

    wrote MoveWinch command

--- src/main/java/com/github/mittyrobotics/climber/commands/MoveWinch.java
+++ src/main/java/com/github/mittyrobotics/climber/commands/MoveWinch.java
@@ -7,12 +7,13 @@ import com.revrobotics.CANPIDController;
 import com.revrobotics.ControlType;
 
 
-public class MoveWinch extends CommandBase {    //TODO
+public class MoveWinch extends CommandBase {    //TODO check
 
     private CANPIDController controller;
     private double pos;
+    private double tempPos;
     private RobotSide side;
-
+    private double rampRate = 10;
     public MoveWinch(double pos, RobotSide side) {
         this.pos = pos;
         this.side = side;
@@ -22,11 +23,30 @@ public class MoveWinch extends CommandBase {    //TODO
     @Override
     public void initialize(){
         if (side == RobotSide.LEFT) {
-            controller = new CANPIDController(Winch.getInstance().getLeftSpark());       //TODO move to a command
-            controller.setP(0.5);
-            controller.setI(0);
-            controller.setD(0);
-            controller.setOutputRange(-.5, .5);
+            controller = new CANPIDController(Winch.getInstance().getLeftSpark());
+        } else {
+            controller = new CANPIDController(Winch.getInstance().getRightSpark());
         }
+        controller.setP(0.5);
+        controller.setI(0);
+        controller.setD(0);
+        controller.setOutputRange(-.5, .5);
+    }
+
+    @Override
+    public void execute(){
+        if (side == RobotSide.LEFT) {
+            tempPos = Math.min(pos - Winch.getInstance().getLeftEncoder().getPosition(),
+                    Winch.getInstance().getLeftEncoder().getPosition() + rampRate);
+        } else {
+            tempPos = Math.min(pos - Winch.getInstance().getRightEncoder().getPosition(),
+                    Winch.getInstance().getRightEncoder().getPosition() + rampRate);
+        }
+            controller.setReference(tempPos, ControlType.kPosition);
+    }
+
+    @Override
+    public boolean isFinished(){
+        return Math.abs(pos - Winch.getInstance().getLeftEncoder().getPosition()) < 1;
     }
 }
