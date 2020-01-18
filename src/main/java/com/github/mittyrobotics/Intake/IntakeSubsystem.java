1649c1c62a276ee1f7fa97951add6eacacf127aa
oliverchen23 <54689665+oliverchen23@users.noreply.github.com>
2020-01-17 17:13:12 -0800

    Stuff

--- src/main/java/com/github/mittyrobotics/Intake/IntakeSubsystem.java
+++ src/main/java/com/github/mittyrobotics/Intake/IntakeSubsystem.java
@@ -1,7 +1,35 @@
 package com.github.mittyrobotics.Intake;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.github.mittyrobotics.OI;
+import com.github.mittyrobotics.conveyor.Constants;
+import com.github.mittyrobotics.conveyor.ConveyorSubsystem;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class IntakeSubsystem extends SubsystemBase {
+    private WPI_TalonSRX talon2;
+
+    private static IntakeSubsystem instance;
+    public static IntakeSubsystem getInstance(){
+        if(instance == null){
+            instance = new IntakeSubsystem();
+        }
+        return instance;
+    }
+    private IntakeSubsystem(){
+        super();
+        setName("Intake");
+    }
+
+    public void initHardware(){
+        talon2 = new WPI_TalonSRX(Constants2.Talon2ID);
+    }
+    public void IntakeBall(){
+        if(ConveyorSubsystem.getInstance().totalBallCount < 5){
+            talon2.set(ControlMode.Velocity, Constants2.Intakespeed);
+        }
+    }
 
 }
