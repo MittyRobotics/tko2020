efa34abac8a17738edd4643302edcc4d17fc4307
arzbshara <54689688+arzbshara@users.noreply.github.com>
2020-01-18 10:53:05 -0800

    Update Constants.java

--- src/main/java/com/github/mittyrobotics/shooter/Constants.java
+++ src/main/java/com/github/mittyrobotics/shooter/Constants.java
@@ -8,4 +8,5 @@ public class Constants {
     public static final double ShooterOutputMin = 0;
     public static final double ShooterOutputMax = 0;
     public static final double ShooterSpeed = .5;
+    public static final double SpeedMultiplier = 0;
 }
