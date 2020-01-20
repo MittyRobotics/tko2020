5f4b2dd452bd2ed0615bcffaf43270ad4c871ca8
Owen Leather <owenleather22@mittymonarch.com>
2020-01-19 17:51:18 -0800

    reformatting

--- src/main/java/com/github/mittyrobotics/turret/ResetTurretEncoder.java
+++ src/main/java/com/github/mittyrobotics/turret/ResetTurretEncoder.java
@@ -5,7 +5,8 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 public class ResetTurretEncoder extends CommandBase {
     private double position; //TODO what is the purpose of position?
     private boolean isDone = false;
-    public ResetTurretEncoder(double position){
+
+    public ResetTurretEncoder(double position) {
         super();
         this.position = position;
         addRequirements(TurretSubsystem.getInstance());
@@ -21,11 +22,11 @@ public class ResetTurretEncoder extends CommandBase {
             isDone = true;
         }
     }
+
     @Override
-    public boolean isFinished(){
+    public boolean isFinished() {
         return isDone;
     }
-
 
 
 }
