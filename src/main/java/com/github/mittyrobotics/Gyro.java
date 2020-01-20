f7110a5986349592464b61a2a36f511be4074fcc
Ashvin Iyer <ashviniyer21@mittymonarch.com>
2020-01-20 07:59:23 -0800

    Adding 360 angle

--- src/main/java/com/github/mittyrobotics/Gyro.java
+++ src/main/java/com/github/mittyrobotics/Gyro.java
@@ -13,4 +13,12 @@ public class Gyro extends ADXRS450_Gyro {
 	private Gyro(){
 		super();
 	}
+	private double getAngle360(){
+		double angle = getAngle();
+		angle %= 360;
+		if(angle < 0){
+			angle += 360;
+		}
+		return angle;
+	}
 }
