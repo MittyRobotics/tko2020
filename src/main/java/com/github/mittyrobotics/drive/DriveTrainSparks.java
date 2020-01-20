b6fcd8e3731dd6122f15da0a4d9172e9c02bf272
Andy Li <qingyuanli23@mittymonarch.com>
2020-01-20 11:07:40 -0800

    ashvin

--- src/main/java/com/github/mittyrobotics/drive/DriveTrainSparks.java
+++ src/main/java/com/github/mittyrobotics/drive/DriveTrainSparks.java
@@ -36,17 +36,21 @@ public class DriveTrainSparks extends SubsystemBase {
     public void initHardware(){
         leftSpark1 = new CANSparkMax(Constants.LEFT_SPARK_1_ID, CANSparkMaxLowLevel.MotorType.kBrushless);
         leftSpark2 = new CANSparkMax(Constants.LEFT_SPARK_2_ID, CANSparkMaxLowLevel.MotorType.kBrushless);
-        leftSpark2.follow(leftSpark1);
 
         rightSpark1 = new CANSparkMax(Constants.RIGHT_SPARK_1_ID, CANSparkMaxLowLevel.MotorType.kBrushless);
         rightSpark2 = new CANSparkMax(Constants.RIGHT_SPARK_2_ID, CANSparkMaxLowLevel.MotorType.kBrushless);
-        rightSpark2.follow(rightSpark1);
 
 
         leftSpark1.restoreFactoryDefaults();
         leftSpark2.restoreFactoryDefaults();
         rightSpark1.restoreFactoryDefaults();
         rightSpark2.restoreFactoryDefaults();
+        rightSpark1.follow(leftSpark1);
+        rightSpark2.follow(rightSpark1);
+        leftSpark1.setMotorType(CANSparkMaxLowLevel.MotorType.kBrushless);
+        leftSpark2.setMotorType(CANSparkMaxLowLevel.MotorType.kBrushless);
+        rightSpark1.setMotorType(CANSparkMaxLowLevel.MotorType.kBrushless);
+        rightSpark2.setMotorType(CANSparkMaxLowLevel.MotorType.kBrushless);
         rightSpark1.setInverted(true);
         rightSpark2.setInverted(true);
         setDefaultCommand(new Drive());
@@ -55,19 +59,15 @@ public class DriveTrainSparks extends SubsystemBase {
     public void tankDrive(double left, double right) {
         if (Math.abs(left) > 0.1){
             leftSpark1.set(left);
-            leftSpark2.set(left);
         } else {
             leftSpark1.stopMotor();
-            leftSpark2.stopMotor();
 
         }
 
         if (Math.abs(right) > 0.1){
             rightSpark1.set(right);
-            rightSpark2.set(right);
         } else {
             rightSpark1.stopMotor();
-            rightSpark2.stopMotor();
         }
     }
 
