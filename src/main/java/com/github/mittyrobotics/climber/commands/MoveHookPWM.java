6c77ed083d9d89febcbab9fceb9e145c3ccc7c05
Ashvin Iyer <ashviniyer21@mittymonarch.com>
2020-01-24 16:19:39 -0800

    Comments for PWM Piston

--- src/main/java/com/github/mittyrobotics/climber/commands/MoveHookPWM.java
+++ src/main/java/com/github/mittyrobotics/climber/commands/MoveHookPWM.java
@@ -11,10 +11,10 @@ import java.util.Calendar;
 
 public class MoveHookPWM extends CommandBase {
 
-    RobotSide side;
+    RobotSide side; //TODO make all of these private
     ElevateDirection direction;
     double activePercent, inactivePercent, startTime;
-    boolean toggle;
+    boolean toggle; //TODO what is this for?
 
     public MoveHookPWM(RobotSide side, ElevateDirection direction) {
         super();
@@ -24,15 +24,15 @@ public class MoveHookPWM extends CommandBase {
     }
 
     @Override
-    public void initialize() {
+    public void initialize() { //TODO make this a paramter
         activePercent = Constants.PISTON_ACTIVE_PERCENT;
         inactivePercent = 1 - Constants.PISTON_ACTIVE_PERCENT;
-        startTime = Calendar.getInstance().getTimeInMillis();
+        startTime = Calendar.getInstance().getTimeInMillis(); //TODO DO NOT USE CALENDAR. Use Timer.getFPGATimestamp() (it is frc's timer)
     }
 
     @Override
-    public void execute() {
-        if(Calendar.getInstance().getTimeInMillis() - startTime > activePercent * 100) {
+    public void execute() { //TODO You never set it to go off when it is not active
+        if(Calendar.getInstance().getTimeInMillis() - startTime > activePercent * 100) { //TODO not sure how this logic performs PWM
             Hooks.getInstance().push(side, direction);
         }
     }
