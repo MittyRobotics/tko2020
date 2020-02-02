da6f0e766730eabddd7695b5d84e3803b5c26560
Owen Leather <owenleather@mittymonarch.com>
2020-02-02 14:15:46 -0800

    Update AutonConstants.java

--- src/main/java/com/github/mittyrobotics/autonomous/constants/AutonConstants.java
+++ src/main/java/com/github/mittyrobotics/autonomous/constants/AutonConstants.java
@@ -26,7 +26,7 @@ package com.github.mittyrobotics.autonomous.constants;
 
 public class AutonConstants {
     //Field
-    public static double HIGH_TARGET_HEIGHT = 92; /*(6.836 + 9.438) / 2.0;*/ //ft //TODO: Calibrate to field
+    public static double HIGH_TARGET_HEIGHT = (6.836 + 9.438) / 2.0; /*(6.836 + 9.438) / 2.0;*/ //ft //TODO: Calibrate to field
     public static double HIGH_TARGET_MIN_HEIGHT = 6.938 + (7.0 / 12.0 / 2.0); //ft
     public static double HIGH_TARGET_MAX_HEIGHT = 9.438 - (7.0 / 12.0 / 2.0); //ft
 
