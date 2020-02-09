6a0726388cd1e5538832f87009dac136cd74883b
mkorolik <mariakorolik21@mittymonarch.com>
2020-02-09 12:17:43 -0800

    just one (1) comment

--- src/main/java/com/github/mittyrobotics/climber/commands/MoveWinchGroupUp.java
+++ src/main/java/com/github/mittyrobotics/climber/commands/MoveWinchGroupUp.java
@@ -7,6 +7,7 @@ import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 public class MoveWinchGroupUp extends ParallelCommandGroup {
 
     public MoveWinchGroupUp(){
+        //TODO make sure intake is extended
         double pos = Constants.PISTON_DISTANCE;
         addCommands(new MoveWinch(pos, RobotSide.LEFT), new MoveWinch(pos, RobotSide.RIGHT));
     }
