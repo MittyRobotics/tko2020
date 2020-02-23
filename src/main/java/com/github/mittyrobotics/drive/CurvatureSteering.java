14e2b7b2b4386636efe9afd12aaa08062f3e3fde
Ishaan Sathaye <IshaanSathaye21@mittymonarch.com>
2020-02-22 19:51:49 -0800

    added a button for reversing drive

--- src/main/java/com/github/mittyrobotics/drive/CurvatureSteering.java
+++ src/main/java/com/github/mittyrobotics/drive/CurvatureSteering.java
@@ -17,13 +17,13 @@ public class CurvatureSteering extends CommandBase {
     }
 
     @Override
-    public void execute(){
+    public void execute() {
 
-        double turn = (OI.getInstance().getXboxWheel().getX()*450); //steering wheel
+        double turn = (OI.getInstance().getXboxWheel().getX() * 450); //steering wheel
         double joystickSpeed = -OI.getInstance().getJoystick1().getY(); //joystick
         boolean brake = OI.getInstance().getJoystick1().getTrigger(); //brake
         final double radiusE = 5;
-        double radius = Math.abs(450/turn * radiusE); //radius of imaginary circle
+        double radius = Math.abs(450 / turn * radiusE); //radius of imaginary circle
         double halfWidthRobot = 12.625; //12.5 for falcon
 
         double leftSpeed = 0;
@@ -32,18 +32,18 @@ public class CurvatureSteering extends CommandBase {
         boolean inThreshold = Math.abs(turn) < threshold;
         double turnScale = 1;
         //testing for 10 degrees to the left
-        if (turn >= threshold){
-            leftSpeed = (radius + halfWidthRobot)*(2*Math.PI);
-            rightSpeed = (radius - halfWidthRobot)*(2*Math.PI);
-            rightSpeed = (rightSpeed/leftSpeed) * turnScale ;
+        if (turn >= threshold) {
+            leftSpeed = (radius + halfWidthRobot) * (2 * Math.PI);
+            rightSpeed = (radius - halfWidthRobot) * (2 * Math.PI);
+            rightSpeed = (rightSpeed / leftSpeed) * turnScale;
             leftSpeed = 1 * turnScale;
-        } else if (turn <= -threshold){
-            leftSpeed = (radius - halfWidthRobot)*(2*Math.PI);
-            rightSpeed = (radius + halfWidthRobot)*(2*Math.PI);
-            leftSpeed = (leftSpeed/rightSpeed) * turnScale;
+        } else if (turn <= -threshold) {
+            leftSpeed = (radius - halfWidthRobot) * (2 * Math.PI);
+            rightSpeed = (radius + halfWidthRobot) * (2 * Math.PI);
+            leftSpeed = (leftSpeed / rightSpeed) * turnScale;
             rightSpeed = 1 * turnScale;
         } else if (inThreshold) {
-            turn = OI.getInstance().getXboxWheel().getX()*450/120;
+            turn = OI.getInstance().getXboxWheel().getX() * 450 / 120;
         }
 
         if (brake) {
@@ -53,15 +53,19 @@ public class CurvatureSteering extends CommandBase {
             joystickSpeed = 0;
         }
 
-        if(OI.getInstance().getJoystick1().getRawButtonPressed(2)){
-            isReversed = !isReversed;
+        if (OI.getInstance().getJoystick1().getRawButtonPressed(2)) {
+            isReversed = true;
+        } else if (OI.getInstance().getJoystick1().getRawButtonPressed(1)) { //change button based on kito's preference
+            isReversed = false;
         }
+
         if(isReversed){
             joystickSpeed = -joystickSpeed;
             double temp = leftSpeed;
             leftSpeed = rightSpeed;
             rightSpeed = temp;
         }
+        
         if (inThreshold){
             //DriveTrainTalon.getInstance().tankDrive(joystickSpeed, joystickSpeed);
             DriveTrainFalcon.getInstance().tankDrive(
