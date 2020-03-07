19efadfcf25b3da9dcaeb220a411204dc3e7170e
Ashvin Iyer <ashviniyer21@mittymonarch.com>
2020-03-07 13:22:52 -0800

    Cleaning up Conveyor Code

--- src/main/java/com/github/mittyrobotics/conveyor2/Conveyor2Constants.java
+++
@@ -1,5 +0,0 @@
-package com.github.mittyrobotics.conveyor2;
-
-public class Conveyor2Constants {
-    public static double TICKS_PER_CYCLE = 52900;
-}
--- src/main/java/com/github/mittyrobotics/conveyor2/ConveyorMoveCommand.java
+++
@@ -1,9 +0,0 @@
-package com.github.mittyrobotics.conveyor2;
-
-import edu.wpi.first.wpilibj2.command.RunCommand;
-
-public class ConveyorMoveCommand extends RunCommand {
-    public ConveyorMoveCommand(double speed) {
-        super(()->Conveyor2Subsystem.getInstance().setMotor(speed), Conveyor2Subsystem.getInstance());
-    }
-}
