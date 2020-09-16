29ef43d5037c6657b299e042439a75cce38975b9
Ashvin Iyer <ashviniyer21@mittymonarch.com>
2020-09-16 13:47:54 -0700

    Removing old climber code

--- src/main/java/com/github/mittyrobotics/commands/WindWinchCommand.java
+++
@@ -1,9 +0,0 @@
-package com.github.mittyrobotics.commands;
-
-import com.github.mittyrobotics.constants.ClimberConstants;
-
-public class WindWinchCommand extends MoveWinchCommand {
-    public WindWinchCommand() {
-        super(ClimberConstants.CLIMBER_DOWN_POSITION, ClimberConstants.CLIMBER_DOWN_POSITION);
-    }
-}
