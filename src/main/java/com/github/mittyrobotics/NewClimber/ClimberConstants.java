df589cc61d9629bb634cabfae315266c3fa1c2c0
Vidit567 <43189246+Vidit567@users.noreply.github.com>
2020-09-24 15:27:20 -0700

    fixed the climber

--- src/main/java/com/github/mittyrobotics/NewClimber/ClimberConstants.java
+++ src/main/java/com/github/mittyrobotics/NewClimber/ClimberConstants.java
@@ -5,4 +5,5 @@ public class ClimberConstants {
     public static final int RIGHT_SPARK_ID = 32;
     public static final boolean LEFT_SPARK_INVERSION = true;
     public static final boolean RIGHT_SPARK_INVERSION = false;
+    public static final double ARM_SETPOINT = 0;
 }
--- src/main/java/com/github/mittyrobotics/NewClimber/ClimberSubsystem.java
+++ src/main/java/com/github/mittyrobotics/NewClimber/ClimberSubsystem.java
@@ -1,14 +1,17 @@
 package com.github.mittyrobotics.NewClimber;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.github.mittyrobotics.constants.ShooterConstants;
 import com.github.mittyrobotics.util.interfaces.IMotorSubsystem;
 import com.revrobotics.*;
+import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 /**
  * Climber subsystem with 2 sparks
  */
+
 public class ClimberSubsystem extends SubsystemBase implements IMotorSubsystem {
     /**
      * {@link ClimberSubsystem} instance
@@ -24,6 +27,14 @@ public class ClimberSubsystem extends SubsystemBase implements IMotorSubsystem {
      * Climber {@link CANEncoder}s
      */
     private CANEncoder leftEncoder, rightEncoder;
+
+    private WPI_TalonSRX talonSRX1, talonSRX2;
+
+    private PIDController primaryController1, primaryController2, auxController;
+
+    private double setpoint;
+
+    private boolean shouldMoveArm;
 
     /**
      * Calls SubsystemBase constructor and names the subsystem 'Climber'
@@ -62,6 +73,15 @@ public class ClimberSubsystem extends SubsystemBase implements IMotorSubsystem {
         climberRightSparkMaster.setInverted(ClimberConstants.RIGHT_SPARK_INVERSION);
         this.rightEncoder = climberRightSparkMaster.getEncoder();
         setDefaultCommand(new StopClimberCommand());
+
+        talonSRX1 = new WPI_TalonSRX(0);
+        talonSRX2 = new WPI_TalonSRX(1);
+        primaryController1 = new PIDController(1, 0, 0);
+        primaryController2 = new PIDController(1, 0, 0);
+        auxController = new PIDController(1, 0, 0);
+        auxController.setSetpoint(0);
+        shouldMoveArm = false;
+        setpoint = ClimberConstants.ARM_SETPOINT;
     }
 
     @Override
@@ -94,5 +114,17 @@ public class ClimberSubsystem extends SubsystemBase implements IMotorSubsystem {
     public void overrideSetMotor(double percent) {
         climberLeftSparkMaster.set(percent);
         climberRightSparkMaster.set(percent);
+    }
+
+    public void periodic(){
+        primaryController1.setSetpoint(setpoint);
+        primaryController2.setSetpoint(setpoint);
+        while (shouldMoveArm){
+            double auxSpeed = auxController.calculate(climberLeftSparkMaster.get() - climberRightSparkMaster.get());
+            double primarySpeed1 = primaryController1.calculate(climberLeftSparkMaster.get());
+            double primarySpeed2 = primaryController2.calculate(climberRightSparkMaster.get());
+            setLeftSpark(primarySpeed1 + auxSpeed);
+            setRightSpark(primarySpeed2 - auxSpeed);
+        }
     }
 }
