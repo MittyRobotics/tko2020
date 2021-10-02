aa7d6e546168c08d83f63622a4f5772e913e106a
AndyLi23 <andyliqy@gmail.com>
2021-10-01 23:56:50 -0700

    change constants (revert if needed!!)

--- src/main/java/com/github/mittyrobotics/drivetrain/DriveConstants.java
+++ src/main/java/com/github/mittyrobotics/drivetrain/DriveConstants.java
@@ -30,7 +30,7 @@ package com.github.mittyrobotics.drivetrain;
 public class DriveConstants {
 
     public static final double DRIVE_THRESHOLD = 0.02;
-    public static final double DRIVE_BOOST = 1.3;
+    public static final double DRIVE_BOOST = 1.4;
     public static final double DRIVE_TRIGGER_THRESHOLD = 0.1;
     public static final double TURN_RATIO = 0.55;
     public static final double FINAL_MULTIPLIER = 0.5;
