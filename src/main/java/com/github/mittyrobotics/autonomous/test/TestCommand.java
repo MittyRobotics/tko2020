dfee9d1c0f931e996e27752c472532fcb222edfe
AndyLi23 <andyliqy@gmail.com>
2021-10-10 22:16:45 -0700

    make drive and vision optional

--- src/main/java/com/github/mittyrobotics/autonomous/test/TestCommand.java
+++ src/main/java/com/github/mittyrobotics/autonomous/test/TestCommand.java
@@ -4,34 +4,46 @@ import com.github.mittyrobotics.autonomous.commands.VisionTurretAim;
 import com.github.mittyrobotics.conveyor.ConveyorSubsystem;
 import com.github.mittyrobotics.conveyor.IntakePistonSubsystem;
 import com.github.mittyrobotics.drivetrain.DrivetrainSubsystem;
-import com.github.mittyrobotics.shooter.ShooterSubsystem;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 
 public class TestCommand extends SequentialCommandGroup {
-    public TestCommand() {
+    public TestCommand(boolean drive, boolean vision) {
+        if(drive) {
+            addCommands(
+                    sequence(
+                            new WaitCommand(1),
+                            new InstantCommand(()-> DrivetrainSubsystem.getInstance().overrideSetMotor(.3, .3), DrivetrainSubsystem.getInstance()),
+                            new WaitCommand(1),
+                            new InstantCommand(()-> DrivetrainSubsystem.getInstance().overrideSetMotor(-.3, -.3), DrivetrainSubsystem.getInstance()),
+                            new WaitCommand(2),
+                            new InstantCommand(()-> DrivetrainSubsystem.getInstance().stopMotor(), DrivetrainSubsystem.getInstance())
+                    )
+            );
+        }
         addCommands(
-            sequence(
-                    new WaitCommand(1),
-                    new InstantCommand(()-> DrivetrainSubsystem.getInstance().overrideSetMotor(.3, .3), DrivetrainSubsystem.getInstance()),
-                    new WaitCommand(1),
-                    new InstantCommand(()-> DrivetrainSubsystem.getInstance().overrideSetMotor(-.3, -.3), DrivetrainSubsystem.getInstance()),
-                    new WaitCommand(2),
-                    new InstantCommand(()-> DrivetrainSubsystem.getInstance().stopMotor(), DrivetrainSubsystem.getInstance()),
-                    new InstantCommand(()-> IntakePistonSubsystem.getInstance().extendPiston(), IntakePistonSubsystem.getInstance()),
-                    new WaitCommand(2),
-                    new InstantCommand(()-> IntakePistonSubsystem.getInstance().retractPiston(), IntakePistonSubsystem.getInstance()),
-                    new WaitCommand(2),
-                    new InstantCommand(() -> ConveyorSubsystem.getInstance().shootBall(), ConveyorSubsystem.getInstance()),
-                    new WaitCommand(2),
-                    new InstantCommand(() -> ConveyorSubsystem.getInstance().outtakeBall(), ConveyorSubsystem.getInstance()),
-                    new WaitCommand(2),
-                    new InstantCommand(() -> ConveyorSubsystem.getInstance().stopMotor(), ConveyorSubsystem.getInstance()),
-                    new TurretTest(0.2),
-                    new TurretTest(-0.2),
-                    new VisionTurretAim(true)
-            )
+                sequence(
+                        new WaitCommand(1),
+                        new InstantCommand(()-> IntakePistonSubsystem.getInstance().extendPiston(), IntakePistonSubsystem.getInstance()),
+                        new WaitCommand(2),
+                        new InstantCommand(()-> IntakePistonSubsystem.getInstance().retractPiston(), IntakePistonSubsystem.getInstance()),
+                        new WaitCommand(2),
+                        new InstantCommand(() -> ConveyorSubsystem.getInstance().shootBall(), ConveyorSubsystem.getInstance()),
+                        new WaitCommand(2),
+                        new InstantCommand(() -> ConveyorSubsystem.getInstance().outtakeBall(), ConveyorSubsystem.getInstance()),
+                        new WaitCommand(2),
+                        new InstantCommand(() -> ConveyorSubsystem.getInstance().stopMotor(), ConveyorSubsystem.getInstance()),
+                        new TurretTest(0.2),
+                        new TurretTest(-0.2)
+                )
         );
+        if(vision) {
+            addCommands(
+                    sequence(
+                        new VisionTurretAim(true)
+                    )
+            );
+        }
     }
 }
