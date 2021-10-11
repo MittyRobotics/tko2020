08696f1f20396c92dcf2ded707a35b55977faf12
AndyLi23 <andyliqy@gmail.com>
2021-10-10 22:29:47 -0700

    add intake

--- src/main/java/com/github/mittyrobotics/autonomous/test/TestCommand.java
+++ src/main/java/com/github/mittyrobotics/autonomous/test/TestCommand.java
@@ -2,7 +2,10 @@ package com.github.mittyrobotics.autonomous.test;
 
 import com.github.mittyrobotics.autonomous.commands.VisionTurretAim;
 import com.github.mittyrobotics.conveyor.ConveyorSubsystem;
+import com.github.mittyrobotics.conveyor.IntakeConstants;
 import com.github.mittyrobotics.conveyor.IntakePistonSubsystem;
+import com.github.mittyrobotics.conveyor.IntakeSubsystem;
+import com.github.mittyrobotics.conveyor.commands.intake.IntakeBallCommand;
 import com.github.mittyrobotics.drivetrain.DrivetrainSubsystem;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
@@ -27,12 +30,18 @@ public class TestCommand extends SequentialCommandGroup {
                         new WaitCommand(1),
                         new InstantCommand(()-> IntakePistonSubsystem.getInstance().extendPiston(), IntakePistonSubsystem.getInstance()),
                         new WaitCommand(2),
+                        new InstantCommand(()-> IntakeSubsystem.getInstance().overrideSetMotor(IntakeConstants.INTAKE_SPEED_FAST), IntakeSubsystem.getInstance()),
+                        new WaitCommand(3),
+                        new InstantCommand(()-> IntakeSubsystem.getInstance().overrideSetMotor(IntakeConstants.OUTTAKE_SPEED), IntakeSubsystem.getInstance()),
+                        new WaitCommand(3),
+                        new InstantCommand(()-> IntakeSubsystem.getInstance().stopMotor(), IntakeSubsystem.getInstance()),
+                        new WaitCommand(1),
                         new InstantCommand(()-> IntakePistonSubsystem.getInstance().retractPiston(), IntakePistonSubsystem.getInstance()),
                         new WaitCommand(2),
                         new InstantCommand(() -> ConveyorSubsystem.getInstance().shootBall(), ConveyorSubsystem.getInstance()),
-                        new WaitCommand(2),
+                        new WaitCommand(3),
                         new InstantCommand(() -> ConveyorSubsystem.getInstance().outtakeBall(), ConveyorSubsystem.getInstance()),
-                        new WaitCommand(2),
+                        new WaitCommand(3),
                         new InstantCommand(() -> ConveyorSubsystem.getInstance().stopMotor(), ConveyorSubsystem.getInstance()),
                         new TurretTest(0.2),
                         new TurretTest(-0.2)
